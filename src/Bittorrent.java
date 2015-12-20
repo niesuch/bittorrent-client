06fe5b2d4c444cf02aad99b498b54f77151ec779
niesuch <niesuch@gmail.com>
2015-12-20 21:15:48 +0100

    Changes in layout

--- src/Bittorrent.java
+++ src/Bittorrent.java
@@ -4,6 +4,9 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -13,15 +16,44 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JToolBar;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
-public class Bittorrent extends JPanel implements ActionListener {
+public final class Bittorrent extends JPanel implements ActionListener {
 
     private final JPanel _infoPanel;
     private static final int _WIDTH = 800;
     private static final int _HEIGHT = 600;
+    private static final String[] buttons = {"Start", "Stop", "Pause", "Delete"};
 
     public Bittorrent() {
         super(new BorderLayout());
+
+        JToolBar toolBar = new JToolBar("Toolbar", JToolBar.VERTICAL);
+        _addButtons(toolBar, buttons);
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            System.err.println("Error loading the appearance of window");
+        }
+
+        JButton stopButton = new JButton("Cancel");
+        stopButton.setActionCommand("CANCEL");
+        stopButton.addActionListener(this);
+
+        JButton startButton = new JButton("Start");
+        startButton.setActionCommand("START");
+        startButton.addActionListener(this);
+
+        JButton pauseButton = new JButton("Pause");
+        pauseButton.setActionCommand("PAUSE");
+        pauseButton.addActionListener(this);
+        
+        JButton deleteButton = new JButton("Delete");
+        deleteButton.setActionCommand("DELETE");
+        deleteButton.addActionListener(this);
 
         JMenuBar menubar = new JMenuBar();
         JMenu menuBittorrentOption = new JMenu("Help");
@@ -29,13 +61,10 @@ public class Bittorrent extends JPanel implements ActionListener {
         menubar.add(menuFileOption);
         menubar.add(menuBittorrentOption);
 
-//      ###### Menu options ######
-//      ** BitTorrent option
         JMenuItem about = new JMenuItem("About");
         about.setActionCommand("ABOUT");
         about.addActionListener(this);
 
-//      ** File option
         JMenuItem openFile = new JMenuItem("Open");
         openFile.setActionCommand("OPEN");
         openFile.addActionListener(this);
@@ -43,7 +72,6 @@ public class Bittorrent extends JPanel implements ActionListener {
         JMenuItem exitApp = new JMenuItem("Exit");
         exitApp.setActionCommand("EXIT");
         exitApp.addActionListener(this);
-//      ###### End of menu options ######
 
         menuBittorrentOption.add(about);
         menuFileOption.add(openFile);
@@ -60,8 +88,44 @@ public class Bittorrent extends JPanel implements ActionListener {
         setPreferredSize(new Dimension(_WIDTH, _HEIGHT));
         add(menubar, BorderLayout.NORTH);
         add(_infoPanel, BorderLayout.SOUTH);
+        add(toolBar, BorderLayout.EAST);
     }
 
+    /**
+     * Function adding all buttons from array
+     * @param toolBar
+     * @param buttons 
+     */
+    private void _addButtons(JToolBar toolBar, String[] buttons) {
+        for (String element : buttons) {
+            JButton button = _makeButton(element);
+            toolBar.add(button);
+        }
+    }
+
+    /**
+     * Function to make buttons
+     * @param name
+     * @return 
+     */
+    private JButton _makeButton(String name) {
+        String imgLocation = "images/icons/" + name + ".png";
+        URL imageURL = Bittorrent.class.getResource(imgLocation);
+        JButton button = new JButton();
+        button.addActionListener(this);
+
+        if (imageURL != null) {
+            button.setIcon(new ImageIcon(imageURL, name));
+        } else {
+            button.setText(name);
+        }
+
+        return button;
+    }
+
+    /**
+     * Function to show application GUI
+     */
     private static void _showGUI() {
         JFrame frame = new JFrame("BitTorrent");
 
@@ -71,6 +135,9 @@ public class Bittorrent extends JPanel implements ActionListener {
         frame.setVisible(true);
     }
 
+    /**
+     * Function to show dialog about authors
+     */
     private static void _showAboutDialog() {
         String authors = "GitHub:"
                 + "\nhttps://github.com/niesuch/bittorrentclient"
@@ -83,10 +150,14 @@ public class Bittorrent extends JPanel implements ActionListener {
 
         final JOptionPane pane = new JOptionPane(authors);
         final JDialog dialog = pane.createDialog((JFrame) null, "About");
-//      dialog.setLocation(10, 10); // if we have set static location
         dialog.setVisible(true);
     }
 
+    /**
+     * Function to open JFileChooser
+     *
+     * @return
+     */
     private String _openFileChooser() {
         JFileChooser fc = new JFileChooser();
         int chooser = fc.showOpenDialog(this);
@@ -98,6 +169,7 @@ public class Bittorrent extends JPanel implements ActionListener {
         return null;
     }
 
+    @Override
     public void actionPerformed(ActionEvent e) {
         String cmd = e.getActionCommand();
 
@@ -111,6 +183,14 @@ public class Bittorrent extends JPanel implements ActionListener {
                     break;
                 case "EXIT":
                     System.exit(0);
+                case "START":
+                    break;
+                case "STOP":
+                    break;
+                case "PAUSE":
+                    break;
+                case "DELETE":
+                    break;
             }
         }
     }
