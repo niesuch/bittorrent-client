007ce2de7744080c6aa12673329600e52381ac5f
Pawonashi <pawonashi@gmail.com>
2016-01-09 16:18:46 +0100

    Update Peer.java

--- Tracker/Peer.java
+++ Tracker/Peer.java
@@ -15,9 +15,14 @@ import java.net.Socket;
 
 public class Peer 
 {
+	public static final String state[] =
+		{
+		    "Unknown", "Started", "Stopped", "Completed"
+		};
 	public int port, fileID;
-	public String id, ip;
+	private String id, ip, peerState;
 	public Socket peerSocket = null;
+	public long downloaded, left;
 	
 	// Constructor
 	
@@ -28,5 +33,40 @@ public class Peer
 		this.port = port;
 		this.fileID = fileID;
 		this.peerSocket = peerSocket;
+		this.peerState = "Unknown";
+		this.downloaded = 0;
+		this.left = 0;
 	}
+	
+	/**
+	 * Updates a state of tracked peer
+	 * @param downloaded how much of a file this peer has downloaded
+	 * @param left how much of a file is left to complete download
+	 */
+	public void update(long downloaded, long left)
+	{
+		if(left == 0)
+			this.peerState = "Completed";
+	}
+	
+	/**
+	 * Return peer's id
+	 * @return id
+	 */
+	public String getPeerId()
+	{
+		return this.id;
+	}
+	
+	/**
+	 * Return peer's ip
+	 * @return ip address
+	 */
+	public String getPeerIp()
+	{
+		return this.ip;
+	}
+	
+	//TODO
+	//updating peer's status, updating peer's information about download
 }
