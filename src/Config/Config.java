64f1333aff5e19c7f1586e54ef7c44b2720ffebb
niesuch <niesuch@gmail.com>
2016-01-15 00:59:26 +0100

    Create Config class to application configs

---
+++ src/Config/Config.java
@@ -0,0 +1,12 @@
+package Config;
+
+/**
+ * Config class - Abstrac class with application configs
+ *
+ * @author Niesuch
+ */
+public abstract class Config
+{
+    public static final int WIDTH = 1024;
+    public static final int HEIGHT = 768;
+}
