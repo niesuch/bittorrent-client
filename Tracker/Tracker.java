13b4feaadf64fdad4ca042778f56a7cfc5003f0f
Pawonashi <pawonashi@gmail.com>
2016-01-22 13:50:11 +0100

    Update Tracker.java
    Added an function for getting ip address of the client launching an bittorrent application

--- Tracker/Tracker.java
+++ Tracker/Tracker.java
@@ -2,9 +2,16 @@ package Tracker;
 
 import java.util.ArrayList;
 import java.util.Timer;
-import java.util.concurrent.locks.ReentrantLock;
 
 import TorrentMetadata.TorrentFile;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Class used for tracking torrent files and peers
@@ -20,17 +27,19 @@ public class Tracker
 		    "Started", "Stopped", "Completed", "Update"
 		};
 	public int interval = 5 * 60;
-	private ReentrantLock trackerLock = new ReentrantLock();
+	//private ReentrantLock trackerLock = new ReentrantLock();
 	private ArrayList<Peer> peers;
 	private ArrayList<InfoHash> torrents = new ArrayList<InfoHash>();
 	public int fileID, userID;
-	public String url;
+        public int port = 6961;
+	public String announceUrl = "/announce";
 	public InfoHash infoHash;
+        
+        private InetSocketAddress address;
 	
 	public Tracker(int fileID)
 	{
 		this.fileID = fileID;
-		// infoHash value should be given here
 		this.userID = 0;
 		this.peers = new ArrayList<Peer>(fileID);
 	}
@@ -38,28 +47,28 @@ public class Tracker
 	/**
 	 * Locks tracker to prevent modification
 	 */
-	public void lock()
-	{
-		this.trackerLock.lock();
-	}
+	//public void lock()
+	//{
+	//	this.trackerLock.lock();
+	//}
 	
 	/**
 	 * Unlocks tracker to make modifications possible
 	 */
-	public void unlock()
-	{
-		this.trackerLock.unlock();
-	}
+	//public void unlock()
+	//{
+	//	this.trackerLock.unlock();
+	//}
 	/**
 	 * Adds new torrent to track
 	 * @param infoHash encoded in SHA1 hash of torrent metafile
 	 */
 	public void add(InfoHash infoHash)
 	{
-		if(this.trackerLock.isHeldByCurrentThread() == true)
-		{
-			System.out.println("Tracker is locked right now.");
-		}
+		//if(this.trackerLock.isHeldByCurrentThread() == true)
+		//{
+		//	System.out.println("Tracker is locked right now.");
+		//}
 		
 		if(infoHash.toString().length() != 20)
 		{
@@ -67,6 +76,29 @@ public class Tracker
 		}
 		
 		this.torrents.add(infoHash);
+	}
+        
+        /**
+         * Gets an ip address of host
+         * @return IP address of host
+         */
+        public String getAnnounceIP() throws MalformedURLException 
+        {
+            Enumeration e;
+            try 
+            {
+                e = NetworkInterface.getNetworkInterfaces();
+                NetworkInterface n = (NetworkInterface) e.nextElement();
+                Enumeration ee = n.getInetAddresses();
+                InetAddress i = (InetAddress) ee.nextElement();
+                return i.getHostAddress();
+            } 
+            catch (SocketException ex) 
+            {
+                Logger.getLogger(Tracker.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            return null;
 	}
 	
 	//TODO
