d9b98cc80a882b2f389f15aab46fc9b78d14cfdd
Pawonashi <pawonashi@gmail.com>
2016-01-26 16:00:21 +0100

    Update Peer.java

--- src/Tracker/Peer.java
+++ src/Tracker/Peer.java
@@ -1,6 +1,9 @@
 package Tracker;
 
 import java.net.Socket;
+import java.util.Date;
+
+import BEncoding.BElement.BDictionary;
 
 /**
  * 
@@ -23,10 +26,12 @@ public class Peer
 	private String id, ip, peerState;
 	public Socket peerSocket = null;
 	public long downloaded, left;
+        private Date lastAnnounce;
+        private InfoHash torrent;
 	
 	// Constructor
 	
-	public Peer(String id, String ip, int port, int fileID, Socket peerSocket)
+	public Peer(String id, String ip, int port, int fileID, Socket peerSocket, InfoHash infoHash)
 	{
 		this.ip = ip;
 		this.id = id;
@@ -36,17 +41,27 @@ public class Peer
 		this.peerState = "Unknown";
 		this.downloaded = 0;
 		this.left = 0;
+                this.lastAnnounce = null;
+                this.torrent = infoHash;
 	}
 	
 	/**
 	 * Updates a state of tracked peer
 	 * @param downloaded how much of a file this peer has downloaded
 	 * @param left how much of a file is left to complete download
+         * @param state represents the state of tracked peer
 	 */
-	public void update(long downloaded, long left)
+	public void update(long downloaded, long left, String state)
 	{
 		if(left == 0)
-			this.peerState = "Completed";
+                {
+                    this.peerState = "Completed";
+                }
+                else
+                {
+                    this.lastAnnounce = new Date();
+                    this.peerState = state;
+                }
 	}
 	
 	/**
@@ -66,7 +81,24 @@ public class Peer
 	{
 		return this.ip;
 	}
-	
-	//TODO
-	//updating peer's status, updating peer's information about download
+        
+        public Integer getPeerPort()
+	{
+		return this.port;
+	}
+        
+        public InfoHash getInfoHash()
+	{
+		return this.torrent;
+	}
+        
+        public BDictionary toDictionary()
+        {
+            BDictionary peer = new BDictionary();
+            peer.Add("ip", this.getPeerIp());
+            peer.Add("port", this.getPeerPort());
+            peer.Add("id", this.getPeerId());
+            
+            return peer;
+        }
 }
